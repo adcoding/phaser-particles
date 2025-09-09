afd983b4f809786e67aaca347d3ad4038a38c919
Adele <adeledendena@gmail.com>
2025-09-09 16:29:43 +0200

    add first particle effect

--- game.js
+++ game.js
@@ -3,11 +3,36 @@ class Game extends Phaser.Scene {
         super({ key: 'Game' });
     }
 
+    preload() {
+        this.load.atlas('flares', 'https://labs.phaser.io/assets/particles/flares.png' , 'https://labs.phaser.io/assets/particles/flares.json');
+    }
+
     create() {
-        this.add.text(400, 300, 'Hello World', {
-            fontSize: '48px',
-            fill: '#fff',
-            fontStyle: 'bold'
+        const emitter = this.add.particles(400, 250, 'flares', {
+            frame: [ 'blue', 'white'],
+            lifespan: 4000,
+            speed: { min: 50, max: 100 },
+            scale: { start: 0.2, end: 0 },
+            gravityY: 150,
+            blendMode: 'ADD',
+            emitting: false
+        });
+
+        this.createButton(emitter);
+    }
+
+    createButton(emitter) {
+        this.button = this.add.rectangle(400, 500, 200, 50, 0x3498db)
+            .setInteractive()
+            .setOrigin(0.5);
+        
+        this.add.text(400, 500, 'Cast Spell', {
+            fontSize: '20px',
+            fill: '#fff'
         }).setOrigin(0.5);
+
+        this.button.on('pointerdown', () => {
+            emitter.explode(16);
+        });
     }
 }
